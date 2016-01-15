dd7489e4d59f02fde101df4e177f716a55ac4301
Andrey Bogdanov <ololoepepe@gmail.com>
2016-01-15 14:01:03 +0500

    Fix

--- bin/clupdate.js
+++ bin/clupdate.js
@@ -69,8 +69,7 @@ var addNewTranslation = function(sourceText, disambiguation) {
         return;
     if (!disambiguation)
         disambiguation = "_default";
-    sourceText = sourceText.replace(/\\n/g, "\n").replace(/\\t/g, "\t").replace(/\\"/g, "\"").replace(/\\'/g, "\'");
-    sourceText = sourceText.replace(/\\`/g, "\`");
+    sourceText = sourceText.replace(/\\n/g, "\n").replace(/\\t/g, "\t").replace(/\\"/g, "\"");
     locales.forEach(function(locale) {
         if (!toTranslate.hasOwnProperty(locale))
             toTranslate[locale] = {};
